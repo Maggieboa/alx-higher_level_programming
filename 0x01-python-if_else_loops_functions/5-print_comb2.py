81e3cb8903119c004b3b03eca840baa896169355
Maggieboa <achymag2020@gmail.com>
2024-02-21 18:22:49 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -5,4 +5,4 @@ for i in range(0, 10):
             if i == 9 and x == 9:
                 print("{}{}\n".format(i, x), end="")
             else:
-                print("{}{}".format(i, x), end=",")
+                print("{}{} ".format(i, x), end=",")
