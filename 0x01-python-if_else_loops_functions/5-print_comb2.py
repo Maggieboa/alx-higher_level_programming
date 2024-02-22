7c1a89538f2e9ed1c0b6041d12eb8ffb5059d0eb
Maggieboa <achymag2020@gmail.com>
2024-02-22 17:37:14 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for j in range(0, 100):
     if j < 99:
-        print("{:02}".format(j), end = ", ")
+        print("{:02}".format(j), end=", ")
     else:
         print("{}".format(j))
