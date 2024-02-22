1d6f5ec47a4e4304530568dc4e8f11ea1c82c60a
Maggieboa <achymag2020@gmail.com>
2024-02-22 16:31:47 +0300

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    if i < 10:
-        for x in range(0, 10):
-            if i == 9 and x == 9:
-                print("{}{}\n".format(i, x), end="")
-            else:
-                print("{}{} ".format(i, x), end=",")
+for j in range(0, 100):
+    if j < 10:
+        print("0{}".format(j), end = ", ")
+    elif j < 99:
+        print("{}".format(j), end = ", ")
+    else:
+        print("{}".format(j))
+    
+       
