78896ef75691a94d752eba5ab0dddd4348665778
maggieboa <achymag2020@gmail.com>
2024-04-25 10:45:54 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv[2];
+if (argv > 0) {
+  let len = 0;
+  let len1 = 0;
+  for (len = 0; len < argv; len++) {
+    for (len1 = 0; len1 < argv; len1++) {
+      process.stdout.write('X');
+    } console.log('');
+  }
+}
