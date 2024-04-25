ec711f96abd2caa7fa6a5b6aa2f71a1c94349755
maggieboa <achymag2020@gmail.com>
2024-04-25 01:14:01 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const argv1 = ['C is function', 'Python is cool', 'JavaScript is amazing'];
+const argv1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 argv1.forEach((item) => {
   console.log(item);
 });
