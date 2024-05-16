51a3510751b6e9f92d544429309e6dc58ed2d493
maggieboa <achymag2020@gmail.com>
2024-05-16 09:08:29 -0700

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square.js');
+class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      for (let x = 0; x < this.height; x++) {
+        let y = 0;
+        let xy = '';
+        while (y < this.width) {
+          xy += 'X';
+          y++;
+        }
+        console.log(xy);
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let j = 0;
+        let ij = '';
+        while (j < this.width) {
+          ij += 'C';
+          j++;
+        }
+        console.log(ij);
+      }
+    }
+  }
+}
+module.exports = Square;
