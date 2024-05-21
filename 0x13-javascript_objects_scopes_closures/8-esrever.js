d1c7761d1ddf6e448ccf27dd0fb62e7abdfa2877
maggieboa <achymag2020@gmail.com>
2024-05-21 04:18:18 -0700

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function rvs (list) {
-  let len = list.length;
+  let len = list.length - 1;
   let count = 0;
 
   while (count < len) {
-    const temp = list[len - 1];
+    const temp = list[len];
     list[len] = list[count];
     list[count] = temp;
     len--;
